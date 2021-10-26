7508692da2746c124e0eae3c4fe755d2bd0fa088
albertodragoni <alberto.1192@gmail.com>
2021-10-26 19:20:21 +0200

    bonus btn dadi

---
+++ dadi/bonus/script.js
@@ -0,0 +1,53 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+let userNum = document.getElementById("user-num");
+let pcNum = document.getElementById("pc-num");
+let result = document.getElementById("result");
+let button = document.querySelector(".btn");
+
+button.addEventListener("click",
+    function(){
+        randUserNum = Math.floor(Math.random() * 6) + 1;
+        userNum.innerHTML = (` UTENTE ${randUserNum}`);
+        
+        randPcNum = Math.floor(Math.random() * 6) + 1;
+        pcNum.innerHTML = (` PC  ${randPcNum}`);
+
+        if(randUserNum > randPcNum){
+            result.innerHTML = "Hai VINTO"
+        } else if (randUserNum < randPcNum) {
+            result.innerHTML = "Hai PERSO"
+        
+        } else{
+            result.innerHTML = "PAREGGIO"
+         
+        }
+        
+    }
+);
+
+
+// CREATE A RANDOM NUMBER (USER)
+// let randUserNum = Math.floor(Math.random() * 6) + 1;
+// console.log("numero user",randUserNum);
+// userNum.innerHTML = (randUserNum);
+
+// CREATE A RANDOM NUMBER (PC)
+// let randPcNum = Math.floor(Math.random() * 6) + 1;
+// console.log("numero pc", randPcNum);
+// pcNum.innerHTML = (randPcNum);
+
+// CHECK RANDOM NUMBERS
+// if(randUserNum > randPcNum){
+//     result.innerHTML = "Hai VINTO"
+// } else if (randUserNum < randPcNum) {
+//     result.innerHTML = "Hai PERSO"
+
+// } else{
+//     result.innerHTML = "PAREGGIO"
+ 
+// }
+
+// PRINT THE RESULT
